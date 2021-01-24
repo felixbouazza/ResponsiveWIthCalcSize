9bf6e2ff2f6fba691f501c39e8e66db56aee4f94
felixbouazza <digitalmine.france@gmail.com>
2021-01-24 18:48:39 +0100

    first and unique commit

---
+++ src/components/Main.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Main.css";
+
+export default function Main() {
+    return (
+        <div id="content">
+            <h2> Votre profil </h2>
+            <div id="content-body">
+                <div className="content-item">
+                    <p>
+                        Votre pseudo : Un pseudo
+                    </p>
+                </div>
+                <div className="content-item">
+                    <p>
+                        Votre Email : Email
+                    </p>
+                </div>
+                <div className="content-item">
+                    <p>
+                        Votre pseudo : Un pseudo
+                    </p>
+                </div>
+                <div className="content-item">
+                    <p>
+                        Votre pseudo : Un pseudo
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Navbar.css"
+
+export default function Navbar() {
+    return (
+        <header id="nav-header">
+            <h1>Welcome on your Dashboard</h1>
+            <button>Se déconnecter</button>
+        </header>
+    )
+}
